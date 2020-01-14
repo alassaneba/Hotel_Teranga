22696cd29aef0d4ba18d357dbc466586a5d45304
Al Assane BA <alassaneba9559@gmail.com>
2020-01-14 04:04:19 +0000

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use App\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +31,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env')==='production'){
+          \URL::forceScheme('https');
+        }
     }
 }
